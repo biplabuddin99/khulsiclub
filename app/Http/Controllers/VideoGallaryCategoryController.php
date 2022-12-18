cc2de5500f41876a214e4ebb9783d509e3c6063d
biplabuddin99 <biplabuddin99@github.com>
2022-12-18 16:52:44 +0600

    video gallery done

---
+++ app/Http/Controllers/VideoGallaryCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VideoGallaryCategory;
+use Illuminate\Http\Request;
+
+class VideoGallaryCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\VideoGallaryCategory  $videoGallaryCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(VideoGallaryCategory $videoGallaryCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\VideoGallaryCategory  $videoGallaryCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(VideoGallaryCategory $videoGallaryCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\VideoGallaryCategory  $videoGallaryCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, VideoGallaryCategory $videoGallaryCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\VideoGallaryCategory  $videoGallaryCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(VideoGallaryCategory $videoGallaryCategory)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_12_18_103308_create_video_gallary_captions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_gallary_captions', function (Blueprint $table) {
+            $table->id();
+            $table->string('caption')->nullable();
+            $table->string('feature_image')->nullable();
+            $table->string('video_id')->nullable();
+            $table->date('publish_date')->nullable();
+            $table->date('unpublished_date')->nullable();
+            $table->string('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_gallary_captions');
+    }
+};
