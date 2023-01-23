11633a8e9015771874a8a4edd3662a9a8314c689
Nasim Ahmed <nasimn01@githup.com>
2023-01-23 22:24:10 +0600

    update facilities

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -400,7 +400,7 @@
           items : 3,
           itemsDesktop : [1199,4],
           itemsDesktopSmall : [980,3],
-          itemsTablet: [768,2],
+          itemsTablet: [768,1],
           itemsMobile : [479,1]
         })
 
