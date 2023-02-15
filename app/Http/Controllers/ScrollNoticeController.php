fe278e624d4c120f169f2a5f0006d11a25c8a488
Nasim Ahmed <nasimn01@githup.com>
2023-02-15 21:59:50 +0600

    create scroll notice table

---
+++ app/Http/Controllers/ScrollNoticeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\scroll_notice;
+use Illuminate\Http\Request;
+
+class ScrollNoticeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\scroll_notice  $scroll_notice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(scroll_notice $scroll_notice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\scroll_notice  $scroll_notice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(scroll_notice $scroll_notice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\scroll_notice  $scroll_notice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, scroll_notice $scroll_notice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\scroll_notice  $scroll_notice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(scroll_notice $scroll_notice)
+    {
+        //
+    }
+}
