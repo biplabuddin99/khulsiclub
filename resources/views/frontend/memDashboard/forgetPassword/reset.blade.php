5eaec4d08ca1db3cb8c61f4648e2b97a2bd0c068
Nasim Ahmed <nashim.idb333@gmail.com>
2024-03-24 14:22:00 +0600

    updated

--- resources/views/frontend/memDashboard/forgetPassword/reset.blade.php
+++ resources/views/frontend/memDashboard/forgetPassword/reset.blade.php
@@ -9,25 +9,6 @@
                 @if(Session::has('response'))
                     {!!Session::get('response')['message']!!}
                 @endif
-                @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-                @if(session()->has('success'))
-                    <div class="alert alert-success">
-                        {{ session()->get('success') }}
-                    </div>
-                @endif
-                @if(session()->has('error'))
-                    <div class="alert alert-danger">
-                        {{ session()->get('error') }}
-                    </div>
-                @endif
               <div class="row">
                     <div class="col-lg-4 logo-side-section">
                         <div class="loginSideText h-100 ">
